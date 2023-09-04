099d0b2a90bf32c31426616627a8d87d7c08b2e7
Rylexium <aleksei.azizov@mail.ru>
2023-09-04 23:50:33 +0400

    beta for v.24

--- InvertTheme.js
+++ InvertTheme.js
@@ -60,6 +60,14 @@ function awaitElements(selector, lambdaFunction){
      }, 100)
 }
 
+function trySetAtrributes(lambda) {
+    try {
+        lambda()
+    } catch (error) {
+        
+    }
+}
+
 
 async function checkVersion() {
     return await new Promise(resolve => {
@@ -85,19 +93,28 @@ checkVersion().then((versionSIEM)=>{
         })
         //Select field in navbar
         addCSSRule(".mc-navbar-item.mc-active, .mc-navbar-brand.mc-active, .mc-navbar-toggle.mc-active", {"-webkit-filter": "invert(1)"})
+        
+        // setInterval(function() {
+        //     document.querySelectorAll('.pt-icons').forEach((elem)=>{ elem.style.filter='invert(1)'})
+        // }, 100)
 
-        //event scroll, here change all elements
-        document.querySelector("#\\31 693764353912-body-grid-container > div.ui-grid-viewport.ng-isolate-scope").addEventListener("scroll", function(){console.log("scroll!!")})
-
-
-        // // //онли значки меняем
-        // awaitElements('.pt-icons', ()=>{
-        //     document.styleSheets[1].insertRule(addAttributeToStyle('.pt-icons', "-webkit-filter", "invert(1)"), 0);
+        // awaitElements(".pt-icons", ()=>{
+        //     document.querySelectorAll('.pt-icons').forEach((elem)=>{ elem.style.filter='invert(1)'})
+        // })
+        // awaitElements(".mc-label", ()=>{
+        //     document.querySelectorAll('.mc-label').forEach((elem)=>{ elem.style.filter='invert(1)'})
+        // })
+        // awaitElements(".mc", ()=>{
+        //     document.querySelectorAll('.mc').forEach((elem)=>{ elem.style.filter='invert(1)'})
+        // })
+        // awaitElements(".mc, .pt-icons", ()=>{
+        //     document.querySelectorAll('.mc, .pt-icons').forEach((elem)=>{ elem.style.filter='invert(1)'})
         // })
 
-        //.mc-label
-        //.mc
-        //.mc, .pt-icons
+        //event scroll, here change all elements
+        // document.querySelector("#\\31 693764353912-body-grid-container > div.ui-grid-viewport.ng-isolate-scope").addEventListener("scroll", function(){console.log("scroll 0")})
+        // document.querySelector("body > section > div > div > events-page > div > section > mc-sidebar.mc-sidebar_right.ng-scope.ng-isolate-scope > mc-sidebar-opened > event-info > section > section").addEventListener("scroll", function(){console.log("scroll 1")})
+
         // addCSSRule(".pt-icons", {"-webkit-filter": "invert(1)"})
         // addCSSRule(".mc-check_16", {"-webkit-filter": "invert(1)"})
 
@@ -142,11 +159,63 @@ checkVersion().then((versionSIEM)=>{
 
 
     } else {
-        awaitElements("nav", ()=>{
-            document.querySelector("nav").style.background=titleBackgroundColor
-            document.querySelector("nav").style.filter=invert
-        })
+        setInterval(function() {
+            
+            document.querySelector(".mc-navbar").style.background='white'
+            document.querySelectorAll('.mc-navbar.navbar-blue .mc-navbar__button, .mc-navbar.navbar-blue .mc-navbar__link').forEach((elem)=>{elem.style.filter='invert(1)'})
+            document.querySelectorAll(".mc-navbar .mc-icon, .mc-navbar .mc-navbar-title").forEach((elem)=>{elem.style.filter='invert(1)'})
 
+            document.querySelector(".mc-navbar").querySelectorAll('.pt-icons').forEach((elem)=>{elem.style.filter='invert(0)'})
+
+            awaitElements('body > section > div', ()=>{ document.querySelector(".pt-navbar-icon").style.filter='invert(1)'})
+            awaitElements('mc-navbar-logo', ()=>{ document.querySelector("mc-navbar-logo").style.filter='invert(1)'})
+
+            document.querySelector("body > section > nav-bar > nav > div > div.mc-navbar__left.layout-row > div.mc-navbar__header.pt-text-overflow.flex-nogrow.ng-isolate-scope").style.filter='invert(1)'
+            document.querySelector('.mc-navbar.navbar-blue .mc-navbar__item.mc-active, .mc-navbar.navbar-blue .mc-navbar__item.mc-navbar__item_active, .mc-navbar.navbar-blue .mc-navbar__item.open').style.background='grey'
+            trySetAtrributes(()=>{ document.querySelector("mc-navbar-brand").style.filter='invert(1)'})
+            
+
+            //.mc-dl .mc-dt, .mc-dl dt, dl .mc-dt, dl dt
+            trySetAtrributes(()=>{ 
+                document.querySelector("body > knowledge-base-root > ng-component > div").querySelectorAll('.pt-icons').forEach((elem)=>{ elem.style.filter='invert(1)'})
+            })
+
+            trySetAtrributes(()=>{ document.querySelector("body > section > div").querySelectorAll('.pt-icons').forEach((elem)=>{ elem.style.filter='invert(1)'}) })
+            trySetAtrributes(()=>{ document.querySelector("body > section > div").querySelectorAll('.mc-label').forEach((elem)=>{ elem.style.filter='invert(1)'}) })
+            trySetAtrributes(()=>{ document.querySelector("body > section > div").querySelectorAll('.mc').forEach((elem)=>{ elem.style.filter='invert(1)'}) })
+            trySetAtrributes(()=>{ document.querySelector("body > section > div").querySelectorAll('.mc, .pt-icons').forEach((elem)=>{ elem.style.filter='invert(1)'}) })
+            
+
+            document.querySelectorAll('.mc-switch.mc-checked').forEach((elem)=>{
+                elem.style.filter=invert
+            })
+
+            //charts
+            document.querySelectorAll(".highcharts-point").forEach((elem)=>{elem.style.filter='invert(1)'})
+
+            //health monitor
+            document.querySelectorAll("div.notifications__item").forEach((elem=>{ elem.style.filter='invert(1)' }))
+            document.querySelectorAll('.notifications__message').forEach((elem=>{ elem.style.filter='invert(1)' }))
+            //document.querySelectorAll("notifications .notifications__item_warning").forEach((elem=>{ elem.style.filter='invert(1)' }))
+            //document.querySelector("div.notifications__message").style.filter='invert(1)'
+
+            //document.querySelector(".mc-navbar.navbar-blue").querySelectorAll('.pt-icons').forEach((elem)=>{elem.style.filter='invert(1)'})
+            //document.querySelector(".mc-navbar.navbar-blue").querySelectorAll('.ng-scope').forEach((elem)=>{elem.style.filter='invert(1)'})
+
+            //document.querySelector(".mc-navbar.navbar-blue").style.background='black'
+            //document.querySelectorAll('a').forEach((elem)=>{
+            //     elem.style.filter='invert(1)'
+            // })
+
+
+            //document.querySelector('.mc-navbar.navbar-blue').style.filter=invert
+            //document.querySelector(".mc-navbar.navbar-blue").style.background='white'
+            // awaitElements("body > section > nav-bar", ()=>{
+            //     document.querySelector("body > section > nav-bar").style.background=titleBackgroundColor
+            //     document.querySelector("body > section > nav-bar").style.filter=invert
+            //     document.querySelector("mc-btn mc-navbar__button ng-scope mc-active").style.filter="invert(1)"
+            // })
+        }, 100)
 
     }
 })
